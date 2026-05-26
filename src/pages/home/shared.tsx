25c53ac2b66160beb54f36899ef4e500d343c39e
zhangjc5 <zhangjc5@gitlab.asiainfo-sec.com>
2026-05-26 16:31:03 +0800

    feat(home): add shared HomeViewProps type for 4 theme sub-components

---
+++ src/pages/home/shared.tsx
@@ -0,0 +1,15 @@
+import type { Trip } from '../../types/trip'
+
+/** 4 主题子组件共用的 props */
+export interface HomeViewProps {
+  trips: Trip[]
+  loading: boolean
+  openid: string
+  onOpenTrip: (trip: Trip) => void
+  onLongPressTrip: (trip: Trip) => void
+  onNewTrip: () => void
+  onAITrip: () => void
+  /** featureTrip 由父组件挑选（杂志主题），其他主题忽略 */
+  featureTrip?: Trip
+  onCoverLongPress?: (trip: Trip) => void
+}
